a92a3313ae5948b7c36f1f2004c4c565e910c4b9
oleg <voronin_oleg@live.ru>
2023-02-02 14:58:34 +0300

    Добавлена возможность осреднения за n суток

--- constants.py
+++ constants.py
@@ -9,5 +9,4 @@ else:
 start_dir = path.abspath(curdir)
 if not path.exists(start_dir + sl + 'Logs'):
     mkdir(start_dir + sl + 'Logs')
-# local_dir = f'{start_dir}{sl}Data_base'
-# reg_path = r"SOFTWARE\Oleg_Scripts\Presentations"
+
