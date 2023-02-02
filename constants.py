96b4208cd464b2a45fcb62265ab422eaa88af7b1
oleg <voronin_oleg@live.ru>
2023-02-02 14:56:24 +0300

    Добавлена возможность осреднения за n суток.

--- constants.py
+++ constants.py
@@ -1,5 +1,13 @@
-from os import curdir, path
+from sys import platform
+from os import curdir, path, mkdir
+
+if platform == "win32":
+    sl = '\\'
+else:
+    sl = '/'
 
 start_dir = path.abspath(curdir)
+if not path.exists(start_dir + sl + 'Logs'):
+    mkdir(start_dir + sl + 'Logs')
 # local_dir = f'{start_dir}{sl}Data_base'
 # reg_path = r"SOFTWARE\Oleg_Scripts\Presentations"
