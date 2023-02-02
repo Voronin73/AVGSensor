83b5b4e0949d5be84dd4a2a1818665f56df0c85e
Воронин О.С <voronin_oleg@live.ru>
2023-02-02 15:57:46 +0300

    Добавлена кросплатформенность

--- avg_sensors.py
+++ avg_sensors.py
@@ -2,7 +2,6 @@ from log_files import info
 from functions import sql_request, arguments
 from constants import start_dir
 
-
 # Press the green button in the gutter to run the script.
 
 if __name__ == '__main__':
@@ -16,14 +15,14 @@ if __name__ == '__main__':
        количество строк (не обязательный параметр по умолчанию 3000, чем больше интервал тем больше количестао строк)"""
 
     prev_days, start_time, finish_time, period, avg_time, col_string, timeout = arguments()
-    print(prev_days, start_time, finish_time, period, avg_time, col_string, timeout)
-    # Раскоментировать и ввести необходимые параметры
+
+    # Раскоментировать и ввести необходимые параметры для ручного ввода
     #
     # start_time = '23-02-01 00:00:00'                        # Формат 'YY-MM-DD HH:mm'
     # finish_time = '23-01-31 00:00:00'    # Формат 'YY-MM-DD HH:mm'
     # period = 60*24
-    avg_time = 60*24
-    prev_days = 2
+    # avg_time = 1
+    # prev_days = 2
 
     result = sql_request(
         prev_days=prev_days, time_start=start_time, time_finish=finish_time, period=period,
